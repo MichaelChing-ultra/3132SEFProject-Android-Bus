10e6f1a32338f08d58eb26cc7ad6b04b6481ed97
I-HaVe-Depression <horaceykf.ihvd@gmail.com>
2025-03-31 18:11:31 +0800

    FIXED JavaScript NOT WORKING LETS GO
    FIXED LETSGOOOOOOO

--- app/src/main/java/com/example/busapplication/MainActivity.java
+++ app/src/main/java/com/example/busapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.busapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,12 +8,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView myWebView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
         myWebView.setWebViewClient(new WebViewClient());
         // Load your website URL
         myWebView.loadUrl("file:///android_asset/index.html");
-        myWebView.getSettings().setAllowFileAccess(true);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+        webSettings.setBuiltInZoomControls(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.setSupportZoom(true);
+        webSettings.setDefaultTextEncodingName("utf-8");
+        webSettings.setAllowFileAccess(true);
     }
 }
